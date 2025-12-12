e3530277a8a15e5620d21af45773175874c2dd3a
Vu Quang Hieu <h13uvu12321@gmail.com>
2025-12-13 00:25:35 +0700

    first commit

---
+++ src/test/java/com/shoestore/app/ShoeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoestore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
